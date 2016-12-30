27950f5196f95f217c6442a6c398c0c29a5a977c
aplchian <alex.boquis@gmail.com>
2016-12-29 23:12:15 -0500

    removed excess requires

--- src/function/invoker/index.js
+++ src/function/invoker/index.js
@@ -1,9 +1,4 @@
-const init = require('../../list/init')
 const reverse = require('../../list/reverse')
-const path = require('../../object/path')
-const byIndex = require('../../list/by-index')
-
-
 
 module.exports = (method) => {
   return (...args) => {
